cc64a72223e3eff6b36f338d710e4eb133ea2474
Dev Joseph <esseyjoseph9@gmail.com>
2025-06-23 04:57:49 +0000

    Db setup'

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import app from "./app";
+import pool from "./config/database";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+pool.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("🔴 DB connection failed:", err);
+  });
