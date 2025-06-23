105d001d35e206babce3e479b90cfa315495daf0
Dev Joseph <esseyjoseph9@gmail.com>
2025-06-23 05:16:46 +0000

    authentification

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute";
 
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API running 🚀");
